06fb39df199c50ebc97dc8cbf1343408db9c7bf4
GuanSuns <guansuns@outlook.com>
2018-06-11 00:36:47 -0500

    begin implementing Tamer agent

---
+++ Week 2/ALE Tamer/Python Agents/enum_actions.py
@@ -0,0 +1,15 @@
+# enum_actions.py
+# Author: Lin Guan
+#
+# The Enum structs for actions (UP, LEFT, RIGHT, DOWN)
+
+
+from enum import Enum
+
+
+class Actions(Enum):
+    UP = 2
+    LEFT = 4
+    RIGHT = 3
+    DOWN = 5
+
