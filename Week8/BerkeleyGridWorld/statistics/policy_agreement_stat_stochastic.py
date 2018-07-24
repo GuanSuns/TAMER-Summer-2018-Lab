7db5b3eb39ab61dbbc15b148e09f3f37f5660aa3
GuanSuns <guansuns@outlook.com>
2018-07-24 10:21:19 -0500

    add some experiment logs

--- Week8/BerkeleyGridWorld/statistics/policy_agreement_stat_stochastic.py
+++ Week8/BerkeleyGridWorld/statistics/policy_agreement_stat_stochastic.py
@@ -6,19 +6,45 @@
 import json
 import matplotlib.pyplot as plt
 
-noise_01_experiments = ['/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/08_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/09_agentQ_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/10_agentQ_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/17_agentTamer_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/noise0.1_anneal/avg_policy_agreement_ratio.json']
-noise_01_experiments_names = ['e: 0.1', 'e: 0.3', 'e: 0.5', 'Tamer, e:0.3', 'anneal']
+noise_01_experiments = ['/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/00_agentQ_alpha0.3_epsilon0.01_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/00_agentTamer_alpha0.3_epsilon0.01_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/01_agentQ_alpha0.3_epsilon0.05_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/01_agentTamer_alpha0.3_epsilon0.05_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/02_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/02_agentTamer_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/03_agentQ_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/03_agentTamer_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/04_agentQ_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/04_agentTamer_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/00_VDBE_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.1_speed2.0/avg_policy_agreement_ratio.json']
+noise_01_experiments_names = ['e: 0.01', 'TAMER, e: 0.01', 'e: 0.05', 'TAMER, e: 0.05', 'e: 0.1', 'TAMER, e: 0.1', 'e:0.3', 'TAMER, e:0.3', 'e:0.5', 'TAMER, e:0.5', 'VDBE']
 
-noise_05_experiments = ['/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/11_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/12_agentQ_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/13_agentQ_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/18_agentTamer_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
-    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/noise0.5_anneal/avg_policy_agreement_ratio.json']
-noise_05_experiments_names = ['e: 0.1', 'e: 0.3', 'e: 0.5', 'Tamer, e:0.3', 'anneal']
+
+noise_03_experiments = ['/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/05_agentQ_alpha0.3_epsilon0.01_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/05_agentTamer_alpha0.3_epsilon0.01_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/06_agentQ_alpha0.3_epsilon0.05_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/06_agentTamer_alpha0.3_epsilon0.05_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/07_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/07_agentTamer_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/08_agentQ_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/08_agentTamer_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/09_agentQ_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/09_agentTamer_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/01_VDBE_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.3_speed2.0/avg_policy_agreement_ratio.json']
+noise_03_experiments_names = ['e: 0.01', 'TAMER, e: 0.01', 'e: 0.05', 'TAMER, e: 0.05', 'e: 0.1', 'TAMER, e: 0.1', 'e:0.3', 'TAMER, e:0.3', 'e:0.5', 'TAMER, e:0.5', 'VDBE']
+
+
+noise_05_experiments = ['/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/10_agentQ_alpha0.3_epsilon0.01_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/10_agentTamer_alpha0.3_epsilon0.01_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/11_agentQ_alpha0.3_epsilon0.05_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/11_agentTamer_alpha0.3_epsilon0.05_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/12_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/12_agentTamer_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/13_agentQ_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/13_agentTamer_alpha0.3_epsilon0.3_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/14_agentQ_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json'
+    , '/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/14_agentTamer_alpha0.3_epsilon0.5_policyConverge_synchInput_noise0.5_speed2.0/avg_policy_agreement_ratio.json']
+noise_05_experiments_names = ['e: 0.01', 'TAMER, e: 0.01', 'e: 0.05', 'TAMER, e: 0.05', 'e: 0.1', 'TAMER, e: 0.1', 'e:0.3', 'TAMER, e:0.3', 'e:0.5', 'TAMER, e:0.5']
 
 
 noise_09_experiments = ['/Users/lguan/Documents/Study/Research/Summer 2018/experiment-logs/stochastic-environment/14_agentQ_alpha0.3_epsilon0.1_policyConverge_synchInput_noise0.9_speed2.0/avg_policy_agreement_ratio.json'
@@ -48,7 +74,24 @@ def compare_noise_01():
                 ratio_dict[int(str_key)] = str_ratio_dict[str_key]
             ratios.append(ratio_dict)
     # plot the ratios
-    plotMultipleRatios(ratios, noise_01_experiments_names, 0, 2000)
+    plotMultipleRatios(ratios, noise_01_experiments_names, 0, 200)
+    # wait for user input
+    raw_input("Press Enter to terminate")
+
+
+def compare_noise_03():
+    ratios = list()
+    # read ratios_dict from files
+    for noise_03_experiment in noise_03_experiments:
+        with open(noise_03_experiment) as f_ratios:
+            str_ratio_dict = json.load(f_ratios)
+            ratio_dict = dict()
+            # convert string keys to int keys
+            for str_key in str_ratio_dict:
+                ratio_dict[int(str_key)] = str_ratio_dict[str_key]
+            ratios.append(ratio_dict)
+    # plot the ratios
+    plotMultipleRatios(ratios, noise_03_experiments_names, 0, 2000)
     # wait for user input
     raw_input("Press Enter to terminate")
 
@@ -65,7 +108,7 @@ def compare_noise_05():
                 ratio_dict[int(str_key)] = str_ratio_dict[str_key]
             ratios.append(ratio_dict)
     # plot the ratios
-    plotMultipleRatios(ratios, noise_05_experiments_names, 0, 4000)
+    plotMultipleRatios(ratios, noise_05_experiments_names, 0, 200)
     # wait for user input
     raw_input("Press Enter to terminate")
 
@@ -135,5 +178,5 @@ def plotRatios(ratios, name):
 
 
 if __name__ == '__main__':
-    compare_noise_09()
+    compare_noise_03()
 
